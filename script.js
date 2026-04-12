7b2a485330f8ce3c77a938c767bba691aa476e82
Ifthikar Aboobakker <145112250+ifthikar10@users.noreply.github.com>
2026-04-12 02:13:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,12 @@ document.addEventListener("DOMContentLoaded", () => {
       const questions = encodeURIComponent(card.dataset.questions || "");
       const difficulty = encodeURIComponent(card.dataset.difficulty || "");
       const image = encodeURIComponent(card.dataset.image || "");
+      const quiz = encodeURIComponent(card.dataset.quiz || "travel");
 
-      const url = `instruction.html?title=${title}&time=${time}&questions=${questions}&difficulty=${difficulty}&image=${image}`;
+      const url =
+        `instruction.html?title=${title}&time=${time}&questions=${questions}` +
+        `&difficulty=${difficulty}&image=${image}&quiz=${quiz}`;
+
       window.location.href = url;
     });
   });
